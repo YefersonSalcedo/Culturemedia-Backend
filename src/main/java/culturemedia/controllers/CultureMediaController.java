905d4620d594c54afc72f679ff8f3aa617632d32
Yeferson Salcedo <yeferson.salcedo@udea.edu.co>
2024-05-08 21:33:10 -0500

    Adding Components Rest API and loggers

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -4,21 +4,43 @@ import java.util.List;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
+import culturemedia.repository.Impl.VideoRepositoryImpl;
+import culturemedia.repository.Impl.ViewsRepositoryImpl;
 import culturemedia.service.CultureMediaService;
+import culturemedia.service.Impl.CultureMediaServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class CultureMediaController {
 
 	private final CultureMediaService cultureMediaService;
+	private static final Logger logger = LoggerFactory.getLogger(CultureMediaController.class);
 
 
-	public CultureMediaController(CultureMediaService cultureMediaService) {
-		this.cultureMediaService = cultureMediaService;
+	public CultureMediaController() {
+		this.cultureMediaService = new CultureMediaServiceImpl(new VideoRepositoryImpl(), new ViewsRepositoryImpl());
 	}
 
+	@GetMapping("/videos")
+	public ResponseEntity<List<Video>> findAllVideos() {
 
-	public List<Video> find_allVideos() throws VideoNotFoundException {
-		return cultureMediaService.findAll();
+		try{
+			logger.info("Finding all videos");
+			return ResponseEntity.ok().body(cultureMediaService.findAll());
+		}
+		catch (VideoNotFoundException e) {
+			logger.error("Error finding all videos: {}", e.getMessage());
+			return ResponseEntity.ok().header("Error-Message", e.getMessage()).build();
+		}
 	}
 
+	@PostMapping("/videos")
+	public Video addVideo(@RequestBody Video video) {
+		logger.info("Adding a new video: {}", video.title());
+		return cultureMediaService.save(video);
+	}
 
 }
